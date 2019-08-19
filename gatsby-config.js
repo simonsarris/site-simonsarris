6831f959f44fd4671f7274805713e1551d8b12fa
Simon Sarris <simonsarris@users.noreply.github.com>
2019-08-18 21:26:55 -0400

    yams

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Simon Sarris`,
+    description: `The map is mostly water.`,
+    author: `@simonsarris`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
